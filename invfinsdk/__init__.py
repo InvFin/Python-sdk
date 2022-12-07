490372a78e497f386752e50a195365f16844beda
Lucas <lluc23@hotmail.com>
2022-12-07 19:16:24 +0100

    tweak versionning

--- invfinsdk/__init__.py
+++ invfinsdk/__init__.py
@@ -3,7 +3,7 @@ from .api import Company, Exchange, Industry, Sector, SuperInvestor, Term
 
 __author__ = """Lucas Montes"""
 __email__ = "lluc23@hotmail.com"
-__version__ = __version__
+__version__ = "0.1.1"
 __all__ = [
     "Company",
     "Exchange",
