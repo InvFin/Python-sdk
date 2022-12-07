b5e831bc21b5c4706662c4f47a47515222bb6931
Lucas <lluc23@hotmail.com>
2022-12-07 19:18:46 +0100

    v

--- invfinsdk/__init__.py
+++ invfinsdk/__init__.py
@@ -2,7 +2,7 @@ from .api import Company, Exchange, Industry, Sector, SuperInvestor, Term
 
 __author__ = """Lucas Montes"""
 __email__ = "lluc23@hotmail.com"
-__version__ = "0.1.1"
+__version__ = "0.1.3"
 __all__ = [
     "Company",
     "Exchange",
